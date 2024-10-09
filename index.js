0e9f29ac809ef4b8d97c3b8a788a985248d3e725
passionfirex <126196790+passionfirex@users.noreply.github.com>
2024-10-09 15:00:39 -0400

    one bouncy ball

--- index.js
+++ index.js
@@ -1,1 +1,79 @@
-console.log('Robot Bobby was here');
+import * as THREE from "three";
+import { OrbitControls } from "jsm/controls/OrbitControls.js";
+
+const w = window.innerWidth;
+const h = window.innerHeight;
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(w, h);
+
+document.body.appendChild(renderer.domElement);
+
+// creating scene & camera
+const fov = 75;
+const aspect = w / h;
+const near = 0.1;
+const far = 50;
+const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+camera.position.set(15, 1, 0);
+
+const scene = new THREE.Scene();
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.03
+
+// adding balls 
+function addCircle(posx, posz) {
+    const geo = new THREE.IcosahedronGeometry(1.0, 2);
+    const mat = new THREE.MeshStandardMaterial({
+        color: 0xffffff,
+        flatShading: true
+    });
+    const ball = new THREE.Mesh(geo, mat);
+    ball.position.x = posx;
+    ball.position.z = posz;
+    ball.position.y = 0;
+    ball.name = 'theBall';
+    scene.add(ball);
+};
+
+let ball = addCircle(THREE.MathUtils.randFloatSpread(20), THREE.MathUtils.randFloatSpread(20));
+
+
+function addFloor() {
+    let geometry = new THREE.BoxGeometry(50, 1, 50);
+    let material = new THREE.MeshStandardMaterial({ color: 0xffffff, roughness: 0 });
+    const floor = new THREE.Mesh(geometry, material);
+    floor.position.set(0, -10, 0);
+    floor.name = 'my-floor';
+    scene.add(floor);
+}
+addFloor();
+
+const hemiLight = new THREE.HemisphereLight(0x6f6af7, 0x4bfac3);
+scene.add(hemiLight);
+
+
+// animation numbers 
+let acceleration = 9.8;
+let bounceDistance = 4;
+let bottomPositionY = -8;
+let timeStep = 0.02;
+let timeCounter = Math.sqrt(bounceDistance * 2 / acceleration);
+let initSpeed = acceleration * timeCounter;
+let sphere = scene.getObjectByName('theBall');
+
+
+function animate(ball) {
+    requestAnimationFrame(animate);
+    if (sphere.position.y < bottomPositionY) {
+        timeCounter = 0;
+    }
+    sphere.position.y = bottomPositionY + initSpeed * timeCounter - .5 * acceleration * timeCounter * timeCounter;
+    timeCounter += timeStep;
+
+    renderer.render(scene, camera);
+    controls.update;
+}
+
+animate(ball);
